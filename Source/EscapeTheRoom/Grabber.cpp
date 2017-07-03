c85e3669736dffa44e82dc083552f4bbda60485b
Yuri Levenhagen <yurileven@gmail.com>
2017-07-03 17:17:59 -0300

    Added a visual debugging line to the player

--- Source/EscapeTheRoom/Grabber.cpp
+++ Source/EscapeTheRoom/Grabber.cpp
@@ -1,7 +1,11 @@
 // Yuri Levenhagen 2017
 
 #include "Grabber.h"
+#include "Engine/World.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+#include "Engine.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -9,6 +13,9 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+
+	//The distance which the player can grab stuff
+	Reach = 100.f;
 
 	// ...
 }
@@ -31,6 +38,31 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	FVector PlayerViewPointLocation = FVector();
+	FRotator PlayerViewPointRotation = FRotator();
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation
+	);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), 
+	//	*PlayerViewPointLocation.ToString(),
+	//	*PlayerViewPointRotation.ToString()
+	//);
+
+	//Draw a red trace in the world to visualize
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
 	// ...
 }
 
--- Source/EscapeTheRoom/Grabber.h
+++ Source/EscapeTheRoom/Grabber.h
@@ -24,6 +24,11 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Grabbing")
+	float Reach;
+
 		
 	
 };
